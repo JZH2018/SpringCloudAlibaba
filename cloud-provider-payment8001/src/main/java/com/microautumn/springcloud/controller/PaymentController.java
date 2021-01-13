7b912ed078e214f8d293e0e7a26d54851b53b4b4
JZH2018 <15156581529@163.com>
2021-01-13 22:31:59 +0800

    SpringCloud Alibaba 1-4章零基础入门篇

---
+++ cloud-provider-payment8001/src/main/java/com/microautumn/springcloud/controller/PaymentController.java
@@ -0,0 +1,43 @@
+package com.microautumn.springcloud.controller;
+
+import com.microautumn.springcloud.entities.CommonResult;
+import com.microautumn.springcloud.entities.Payment;
+import com.microautumn.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping("/payment/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("*********插入结果" + result);
+        if (result > 0) {
+            return new CommonResult(200, "录入成功", result);
+        } else {
+            return new CommonResult(444, "录入失败", null);
+        }
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*********查询结果" + payment);
+
+        if (payment != null) {
+            return new CommonResult(200, "查询成功", payment);
+        } else {
+            return new CommonResult(444, "未查寻到记录", null);
+        }
+    }
+
+
+}
