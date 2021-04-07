f997d232893ae8bfa5c5c4f10ff87cb5b49c92af
微秋 <15156581529@163.com>
2021-04-07 20:29:32 +0800

    eureka章节源码

--- cloud-provider-payment8001/src/main/java/com/microautumn/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/microautumn/springcloud/controller/PaymentController.java
@@ -3,11 +3,15 @@ package com.microautumn.springcloud.controller;
 import com.microautumn.springcloud.entities.CommonResult;
 import com.microautumn.springcloud.entities.Payment;
 import com.microautumn.springcloud.service.PaymentService;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,13 +23,15 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("*********插入结果" + result);
         if (result > 0) {
-            return new CommonResult(200, "录入成功,serverPort:"+serverPort, result);
+            return new CommonResult(200, "录入成功,serverPort:" + serverPort, result);
         } else {
             return new CommonResult(444, "录入失败", null);
         }
@@ -38,11 +44,26 @@ public class PaymentController {
         log.info("*********查询结果" + payment);
 
         if (payment != null) {
-            return new CommonResult(200, "查询成功serverPort:"+serverPort, payment);
+            return new CommonResult(200, "查询成功serverPort:" + serverPort, payment);
         } else {
             return new CommonResult(444, "未查寻到记录", null);
         }
     }
 
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*******element: " + element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance i : instances) {
+            log.info(i.getServiceId() + "\t" + i.getHost() + "\t" + i.getPort() + "\t" + i.getUri());
+        }
+        return discoveryClient;
+
+
+    }
 
 }
